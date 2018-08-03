b8d20c5f83f34e6dbf697fd1271ca144d787985a
Rose Reyes <roseannlorinreyes@gmail.com>
2018-08-03 14:30:03 -0700

    made updates

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Hello from './Hello';
+import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
